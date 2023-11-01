fda1d248b734d9e0a46b189d3b5a0aae991b9343
Dominik <lesicadomi22@gmail.com>
2023-11-01 16:12:13 +0100

    Added css to booklist project

--- Booklist/src/Main.jsx
+++ Booklist/src/Main.jsx
@@ -4,7 +4,7 @@ import '../styles/style.scss'
 
 function Book() {
  return (
-  <article>
+  <article className='book'>
     <Image/>
     <Title/>
     <Author/>
@@ -14,16 +14,17 @@ function Book() {
 
 const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/81tdvyI0MeL._AC_UL600_SR600,400_.jpg' alt='Friends, Lovers, and the Big Terrible Thing'/>
 const Title = () => <h2>Friends, Lovers, and the Big Terrible Thing</h2>
-const Author = () => <h4>Matthew Perry</h4>
+const Author = () => <h1>Matthew Perry</h1>
 
 function Booklist() {
-  return (
-    <section>
+  return (<>
+    <h1 className='title'>Best Sellers</h1>
+    <section className='booklist'>
       <Book/>
       <Book/>
       <Book/>
     </section>
-  )
+  </>)
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
