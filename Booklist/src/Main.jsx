2aafd914d57429569df7937a4002aaec4d9ec6af
Dominik <lesicadomi22@gmail.com>
2023-11-01 12:06:53 +0100

    fixed runing bug

--- Booklist/src/Main.jsx
+++ Booklist/src/Main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '../styles/style.scss'
 
-function App() {
-  return <h1>hello</h1>
+function Book() {
+ return (
+  <article>
+    <Image/>
+    <Title/>
+    <Author/>
+  </article>
+ )
+}
+
+const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/81tdvyI0MeL._AC_UL600_SR600,400_.jpg' alt='Friends, Lovers, and the Big Terrible Thing'/>
+const Title = () => <h2>Friends, Lovers, and the Big Terrible Thing</h2>
+const Author = () => <h4>Matthew Perry</h4>
+
+function Booklist() {
+  return (
+    <section>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App/>);
+root.render(<Booklist/>);
