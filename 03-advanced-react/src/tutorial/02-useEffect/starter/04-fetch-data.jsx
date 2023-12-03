acee0350124c4758c6aa814e13e3d795a8effcda
Dominik <lesicadomi22@gmail.com>
2023-12-03 15:34:44 +0100

    Mostly finished useEffect course

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,39 @@
 const url = 'https://api.github.com/users';
+import { useEffect, useState } from "react";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [data, setData] = useState([]);
+  async function fetchData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    setData(data);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <ul className="users">
+    {data.map((userData) => {
+      const {login, avatar_url, html_url} = userData;
+      return <Profile name={login} imgUrl={avatar_url} htmlUrl={html_url}/>
+    })}
+  </ul>;
 };
+
+function Profile({name, imgUrl, htmlUrl}) {
+  return <>
+    <section className="profile">
+      <div className="left-profile">
+        <img src={imgUrl} className="user-img"/>
+      </div> 
+      <div className="right-profile">
+        <h5 className="name">{name}</h5>
+        <a href={htmlUrl}>profile</a>
+      </div>
+    
+
+    </section>
+    
+  </>
+}
 export default FetchData;
