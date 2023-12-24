63079786668c3a38136f482e0ac82d33eabf7580
Dominik <lesicadomi22@gmail.com>
2023-12-24 19:30:07 +0100

    finished project structure course

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,10 +1,9 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
-
+import Example from "./tutorial/04-project-structure/starter/Example/Index";
 
 export default function App() {
   return (
     <div className='container'>
-      <Starter/>
+      <Example />
     </div>
   );
 }
